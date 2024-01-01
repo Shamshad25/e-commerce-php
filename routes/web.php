509c73d4fb04eab887e858e5c5c420519a3fb8ef
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-01 17:48:15 +0530

    Category page created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -33,6 +34,10 @@ Route::group(['prefix' => 'admin'], function(){
 
         Route::get('/dashboard',[HomeController::class,'index'])->name('admin.dashboard');
         Route::get('/logout',[HomeController::class,'logout'])->name('admin.logout');
+
+        // CATEGORY ROUTES
+        Route::get('/categories/create',[CategoryController::class,'create'])->name('categories.create');
+        Route::post('/categories',[CategoryController::class,'store'])->name('categories.store');
 
     });
 
