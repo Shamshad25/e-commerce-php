f0356c53495eac9e9d875c2f3f751c84a1957cef
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-09 12:04:25 +0530

    Front controller updated

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index(){
-        return view('front.home');
+
+        $featuredProducts = Product::where('is_featured','Yes')->orderBy('id','DESC')->where('status',1)->take(8)->get();
+        $data['featuredProducts'] = $featuredProducts;
+
+
+        $latestProducts = Product::orderBy('id','DESC')->where('status',1)->take(8)->get();
+        $data['latestProducts'] = $latestProducts;
+
+        return view('front.home',$data);
     }
 }
