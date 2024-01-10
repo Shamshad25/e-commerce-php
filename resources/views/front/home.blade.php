22879c9e8dee776bf9ff1e5eecba272458ce05bb
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-10 21:35:28 +0530

    Links added

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -139,7 +139,7 @@
                     <div class="col-md-3">
                         <div class="card product-card">
                             <div class="product-image position-relative">
-                                <a href="" class="product-img">
+                                <a href="{{route('front.product',$product->slug)}}" class="product-img">
                                     {{-- <img class="card-img-top" src="{{asset('front-assets/images/product-1.jpg')}}" alt=""> --}}
                                     @if (!empty($productImage->image))
                                         <img class="card-img-top" src="{{asset('uploads/product/small/'.$productImage->image)}}"  />
@@ -186,7 +186,7 @@
                 <div class="col-md-3">
                     <div class="card product-card">
                         <div class="product-image position-relative">
-                            <a href="" class="product-img">
+                            <a href="{{route('front.product',$product->slug)}}" class="product-img">
                                 {{-- <img class="card-img-top" src="{{asset('front-assets/images/product-1.jpg')}}" alt=""> --}}
                                 @if (!empty($productImage->image))
                                     <img class="card-img-top" src="{{asset('uploads/product/small/'.$productImage->image)}}"  />
