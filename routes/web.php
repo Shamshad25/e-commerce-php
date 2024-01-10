c23e1f4e16e1a222f4f15666517a884a7bcf8c24
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-10 21:33:56 +0530

    Product route added

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontController::class,'index'])->name('front.home');
 Route::get('/shop/{categorySlug?}/{subCategorySlug?}', [ShopController::class,'index'])->name('front.shop');
+Route::get('/product/{slug}',[ShopController::class,'product'])->name('front.product');
 
 
 Route::group(['prefix' => 'admin'], function(){
