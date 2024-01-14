83c20da66d56e0ff8d98c70199a04caf99295d53
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-14 22:25:20 +0530

    Product edit update

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -221,6 +221,15 @@
                             </div>
                         </div>
                     </div>
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h2 class="h4 mb-3">Related product</h2>
+                            <div class="mb-3">
+                                
+                                <p class="error"></p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
