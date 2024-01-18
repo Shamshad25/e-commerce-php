e8169a87a9e3206aa1568eebdd4321f15104060e
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-18 23:58:29 +0530

    Add to cart link added

--- resources/views/front/product.blade.php
+++ resources/views/front/product.blade.php
@@ -130,7 +130,8 @@
                                     <a class="whishlist" href="222"><i class="far fa-heart"></i></a>
 
                                     <div class="product-action">
-                                        <a class="btn btn-dark" href="#">
+                                        <a class="btn btn-dark" href="javascritp:void(0);"
+                                            onclick="addToCart({{ $product->id }})">
                                             <i class="fa fa-shopping-cart"></i> Add To Cart
                                         </a>
                                     </div>
@@ -156,23 +157,4 @@
 @endsection
 
 @section('customJs')
-    <script type="text/javascript">
-        function addToCart(id) {
-            $.ajax({
-                url: "{{ route('front.addToCart') }}",
-                type: 'post',
-                data: {
-                    id: id
-                },
-                dataType: 'json',
-                success: function(response) {
-                    if (response.status == true) {
-                        window.location.href = "{{ route('front.cart') }}"
-                    } else {
-                        alert(response.message);
-                    }
-                }
-            });
-        }
-    </script>
 @endsection
