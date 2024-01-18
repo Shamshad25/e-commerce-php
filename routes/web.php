8bb93d49acead1afa5e80b1d59af1e4cfd006312
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-18 23:31:18 +0530

    route updated

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::get('/', [FrontController::class,'index'])->name('front.home');
 Route::get('/shop/{categorySlug?}/{subCategorySlug?}', [ShopController::class,'index'])->name('front.shop');
 Route::get('/product/{slug}',[ShopController::class,'product'])->name('front.product');
 Route::get('/cart', [CartController::class,'cart'])->name('front.cart');
+Route::post('/add-to-cart', [CartController::class,'addToCart'])->name('front.addToCart');
 
 
 
