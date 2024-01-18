af74cdbaf4258247046d0cda202518569b7c73b9
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-18 22:28:00 +0530

    CartController created

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function addToCart()
+    {
+
+    }
+
+    public function cart()
+    {
+
+    }
+}
