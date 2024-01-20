891389f0c63444e98f93e781a8fd5f60a6bca051
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-20 16:49:57 +0530

    Error message added

--- resources/views/front/cart.blade.php
+++ resources/views/front/cart.blade.php
@@ -24,6 +24,14 @@
                         </div>
                     </div>
                 @endif
+                @if (Session::has('error'))
+                    <div class="col-md-12">
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            {{ Session::get('error') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    </div>
+                @endif
                 <div class="col-md-8">
                     <div class="table-responsive">
                         <table class="table" id="cart">
