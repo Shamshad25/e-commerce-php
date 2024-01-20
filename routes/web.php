12e0ff9d405a0b34feb7478c73e5beece4536bed
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-20 17:57:36 +0530

    Delete route added

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::get('/product/{slug}',[ShopController::class,'product'])->name('front.pro
 Route::get('/cart', [CartController::class,'cart'])->name('front.cart');
 Route::post('/add-to-cart', [CartController::class,'addToCart'])->name('front.addToCart');
 Route::post('/update-cart', [CartController::class,'updateCart'])->name('front.updateToCart');
+Route::post('/delete-item', [CartController::class,'deleteItem'])->name('front.deleteItem.cart');
 
 
 
