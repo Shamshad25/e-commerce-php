edfbee24445b9e8b8be48da551fdddf8804b84b3
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-21 22:04:53 +0530

    Register page created and validation added

--- resources/views/front/account/register.blade.php
+++ resources/views/front/account/register.blade.php
@@ -15,23 +15,28 @@
     <section class=" section-10">
         <div class="container">
             <div class="login-form">
-                <form action="/examples/actions/confirmation.php" method="post">
+                <form action="" method="post" name="registrationForm" id="registrationForm">
                     <h4 class="modal-title">Register Now</h4>
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Name" id="name" name="name">
+                        <p></p>
                     </div>
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Email" id="email" name="email">
+                        <p></p>
                     </div>
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Phone" id="phone" name="phone">
+                        <p></p>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Password" id="password" name="password">
+                        <input type="password" class="form-control" placeholder="Password" id="password" name="password">
+                        <p></p>
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Confirm Password" id="cpassword"
-                            name="cpassword">
+                        <input type="password" class="form-control" placeholder="Confirm Password"
+                            id="password_confirmation" name="password_confirmation">
+                        <p></p>
                     </div>
                     <div class="form-group small">
                         <a href="#" class="forgot-link">Forgot Password?</a>
@@ -42,4 +47,66 @@
             </div>
         </div>
     </section>
+@endsection
+@section('customJs')
+    <script type="text/javascript">
+        $('#registrationForm').submit(function(event) {
+            event.preventDefault();
+
+
+            $.ajax({
+                url: "{{ route('account.processRegister') }}",
+                type: 'post',
+                data: $(this).serializeArray(),
+                dataType: 'json',
+                success: function(response) {
+
+                    var errors = response.errors;
+
+                    if (response.status == false) {
+                        if (errors.name) {
+                            $("#name").siblings("p").addClass('invalid-feedback').html(errors.name);
+                            $("#name").addClass('is-invalid');
+                        } else {
+                            $("#name").siblings("p").removeClass('invalid-feedback').html('');
+                            $("#name").removeClass('is-invalid');
+                        }
+
+                        if (errors.email) {
+                            $("#email").siblings("p").addClass('invalid-feedback').html(errors.email);
+                            $("#email").addClass('is-invalid');
+                        } else {
+                            $("#email").siblings("p").removeClass('invalid-feedback').html('');
+                            $("#email").removeClass('is-invalid');
+                        }
+
+                        if (errors.password) {
+                            $("#password").siblings("p").addClass('invalid-feedback').html(errors
+                                .password);
+                            $("#password").addClass('is-invalid');
+                        } else {
+                            $("#password").siblings("p").removeClass('invalid-feedback').html('');
+                            $("#password").removeClass('is-invalid');
+                        }
+                    } else {
+
+                        $("#name").siblings("p").removeClass('invalid-feedback').html('');
+                        $("#name").removeClass('is-invalid');
+
+                        $("#email").siblings("p").removeClass('invalid-feedback').html('');
+                        $("#email").removeClass('is-invalid');
+
+                        $("#password").siblings("p").removeClass('invalid-feedback').html('');
+                        $("#password").removeClass('is-invalid');
+                    }
+
+
+
+                },
+                error: function(jQXHR, exception) {
+                    console.log('somethig went wrong.');
+                }
+            });
+        });
+    </script>
 @endsection
