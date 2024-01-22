6e49d50b6bad7694571584bf2941b02d880d60a7
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-23 00:10:44 +0530

    Auth middleware added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,6 +32,7 @@ class AuthController extends Controller
             $user = new User;
             $user->name = $request->name;
             $user->email = $request->email;
+            $user->phone = $request->phone;
             $user->password = Hash::make($request->password);
             $user->save();
 
