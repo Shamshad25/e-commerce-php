525b2278f8e9212ffdfa5323b4b7544f05797eff
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-01-24 16:34:25 +0530

    Redirect to intended page after login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -59,6 +59,11 @@ class AuthController extends Controller
         if($validator->passes()){
             if(Auth::attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))){
 
+
+                if(session()->has('url.intended')){
+                    return redirect(session()->get('url.intended'));
+                }
+
                 return redirect()->route('account.profile');
 
             }else{
