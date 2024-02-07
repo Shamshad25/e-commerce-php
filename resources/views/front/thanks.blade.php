ac5cac8fc5477300a3a2953522468adbba51b6e2
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-08 00:31:49 +0530

    Thanks blade updated

--- resources/views/front/thanks.blade.php
+++ resources/views/front/thanks.blade.php
@@ -1,4 +1,10 @@
 @extends('front.layouts.app')
 
 @section('content')
+    <section class="container">
+        <div class="col-md-12 text-center py-5">
+            <h1>Thank You!</h1>
+
+        </div>
+    </section>
 @endsection
