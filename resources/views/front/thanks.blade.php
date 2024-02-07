6a2861cd8ba8e6af6419e75918ff61af19d1756b
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-08 00:47:24 +0530

    Thankyou page complete with session message

--- resources/views/front/thanks.blade.php
+++ resources/views/front/thanks.blade.php
@@ -3,8 +3,15 @@
 @section('content')
     <section class="container">
         <div class="col-md-12 text-center py-5">
-            <h1>Thank You!</h1>
 
+            @if (Session::has('success'))
+                <div class="alert alert-success">
+                    {{ Session::get('success') }}
+                </div>
+            @endif
+
+            <h1>Thank You!</h1>
+            <p>Your Order id is: {{ $id }}</p>
         </div>
     </section>
 @endsection
