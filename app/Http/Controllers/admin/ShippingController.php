7a92bd18a3c1296f388c6d3d3896d8319efa23da
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-10 22:25:02 +0530

    create function created in shipping controller

--- app/Http/Controllers/admin/ShippingController.php
+++ app/Http/Controllers/admin/ShippingController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Country;
 use Illuminate\Http\Request;
 
 class ShippingController extends Controller
 {
-    //
+    public function create()
+    {
+        $countries = Country::get();
+    }
 }
