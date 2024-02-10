c75667804a9a05ea1f60557a5aceadce0d8f761f
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-10 22:57:15 +0530

    shipping store method created

--- app/Http/Controllers/admin/ShippingController.php
+++ app/Http/Controllers/admin/ShippingController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Country;
+use App\Models\Shipping;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ShippingController extends Controller
 {
@@ -15,5 +17,32 @@ class ShippingController extends Controller
         $data['countries'] = $countries;
 
         return view('admin.shipping.create', $data);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'country' => 'required',
+            'amount' => 'required|numeric'
+        ]);
+
+        if($validator->passes()){
+
+            $shipping = new Shipping;
+            $shipping->country_id = $request->country;
+            $shipping->amount = $request->amount;
+            $shipping->save();
+
+            session()->flash('success','Shipping added successfully.');
+
+            return response()->json([
+                'status' => true,
+            ]);
+        }else{
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
     }
 }
