aefffada4f3fba22f75c7884051c02e3a77ff7e1
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-10 23:08:54 +0530

    Form submit ajax added

--- resources/views/admin/shipping/create.blade.php
+++ resources/views/admin/shipping/create.blade.php
@@ -19,12 +19,12 @@
     <section class="content">
         <!-- Default box -->
         <div class="container-fluid">
-
+            @include('admin.message')
             <form action="" method="post" name="ShippingForm" id="ShippingForm">
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="mb-3">
                                     <select name="country" id="country" class="form-control">
                                         <option value="">Select a Country</option>
@@ -41,12 +41,17 @@
                             </div>
 
                             <div class="col-md-4">
-                                <input type="text" name="amount" id="amount" class="form-control"
-                                    placeholder="Amount">
+                                <div class="mb-3">
+                                    <input type="text" name="amount" id="amount" class="form-control"
+                                        placeholder="Amount">
+                                    <p></p>
+                                </div>
                             </div>
 
-                            <div class="col-md-2">
-                                <button type="submit" class="btn btn-primary">Create</button>
+                            <div class="col-md-4">
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-primary">Create</button>
+                                </div>
                             </div>
 
                         </div>
@@ -71,7 +76,7 @@
             $("button[type=submit]").prop('disabled', true);
 
             $.ajax({
-                url: '{{ route('categories.store') }}',
+                url: '{{ route('shipping.store') }}',
                 type: 'post',
                 data: element.serializeArray(),
                 dataType: 'json',
@@ -80,34 +85,26 @@
 
                     if (response['status'] == true) {
 
-                        window.location.href = "{{ route('categories.index') }}"
-
-                        $('#name').addClass('is-invalid')
-                            .siblings('p')
-                            .removeClass('invalid-feedback').html('');
-
-                        $('#slug').addClass('is-invalid')
-                            .siblings('p')
-                            .removeClass('invalid-feedback').html('');
+                        window.location.href = "{{ route('shipping.create') }}"
 
                     } else {
                         var errors = response['errors'];
-                        if (errors['name']) {
-                            $('#name').addClass('is-invalid')
+                        if (errors['country']) {
+                            $('#country').addClass('is-invalid')
                                 .siblings('p')
-                                .addClass('invalid-feedback').html(errors['name']);
+                                .addClass('invalid-feedback').html(errors['country']);
                         } else {
-                            $('#name').addClass('is-invalid')
+                            $('#country').addClass('is-invalid')
                                 .siblings('p')
                                 .removeClass('invalid-feedback').html('');
                         }
 
-                        if (errors['slug']) {
-                            $('#slug').addClass('is-invalid')
+                        if (errors['amount']) {
+                            $('#amount').addClass('is-invalid')
                                 .siblings('p')
-                                .addClass('invalid-feedback').html(errors['slug']);
+                                .addClass('invalid-feedback').html(errors['amount']);
                         } else {
-                            $('#slug').addClass('is-invalid')
+                            $('#amount').addClass('is-invalid')
                                 .siblings('p')
                                 .removeClass('invalid-feedback').html('');
                         }
