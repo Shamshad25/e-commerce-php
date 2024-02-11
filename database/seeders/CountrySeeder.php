75e6cf0b82c5cc6956f2227cec0410cb9f71a7e0
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-11 22:01:11 +0530

    Seeder update

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -256,6 +256,7 @@ class CountrySeeder extends Seeder
 			array('code' => 'ZR', 'name' => 'Zaire'),
 			array('code' => 'ZM', 'name' => 'Zambia'),
 			array('code' => 'ZW', 'name' => 'Zimbabwe'),
+            array('code' => 'others', 'name' => 'rest_of_world'),
 		);
 
 		DB::table('countries')->insert($countries);
