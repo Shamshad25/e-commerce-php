16ac787a6214484b83f6e4d2be44d3844d8548ef
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-12 13:17:51 +0530

    Update route added

--- routes/web.php
+++ routes/web.php
@@ -130,6 +130,7 @@ Route::group(['prefix' => 'admin'], function(){
          Route::get('/shipping/create',[ShippingController::class,'create'])->name('shipping.create');
          Route::post('/shipping',[ShippingController::class,'store'])->name('shipping.store');
          Route::get('/shipping/{id}',[ShippingController::class,'edit'])->name('shipping.edit');
+         Route::put('/shipping/{id}',[ShippingController::class,'update'])->name('shipping.update');
 
 
 
