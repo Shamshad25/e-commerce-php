ba0c331c71ad7864608b264e3c0dee5491b3e24e
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-17 23:16:47 +0530

    Cancel link updated

--- resources/views/admin/coupon/create.blade.php
+++ resources/views/admin/coupon/create.blade.php
@@ -130,7 +130,7 @@
 
                 <div class="pb-5 pt-3">
                     <button type="submit" class="btn btn-primary">Create</button>
-                    <a href="{{ route('categories.index') }}" class="btn btn-outline-dark ml-3">Cancel</a>
+                    <a href="{{ route('coupons.index') }}" class="btn btn-outline-dark ml-3">Cancel</a>
                 </div>
 
             </form>
