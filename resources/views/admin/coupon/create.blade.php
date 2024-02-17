d8ce8d7b4589dcbbcf74d3b9fdf96066f204c877
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-17 20:36:13 +0530

    Form created and ajax added for date time picker

---
+++ resources/views/admin/coupon/create.blade.php
@@ -0,0 +1,268 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid my-2">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Create Coupon Code</h1>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <a href="{{ route('categories.index') }}" class="btn btn-primary">Back</a>
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <!-- Default box -->
+        <div class="container-fluid">
+
+            <form action="" method="post" name="categoryForm" id="categoryForm">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="code">code</label>
+                                    <input type="text" name="code" id="code" class="form-control"
+                                        placeholder="Coupon Code">
+                                    <p></p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="name">Name</label>
+                                    <input type="text" name="name" id="name" class="form-control"
+                                        placeholder="Coupon Code Name">
+                                    <p></p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="max_uses">Max Uses</label>
+                                    <input type="number" name="max_uses" id="max_uses" class="form-control"
+                                        placeholder="Max Uses">
+                                    <p></p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="max_uses_user">Max Uses User</label>
+                                    <input type="text" name="max_uses_user" id="max_uses_user" class="form-control"
+                                        placeholder="Max Uses User">
+                                    <p></p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="status">Type</label>
+                                    <select name="status" id="status" class="form-control">
+                                        <option value="percent">Percent</option>
+                                        <option value="fixed">Fixed</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="discount_amount">Discount Amount</label>
+                                    <input type="text" name="discount_amount" id="discount_amount" class="form-control"
+                                        placeholder="Discount Amount">
+                                    <p></p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="min_amount">Min Amount</label>
+                                    <input type="text" name="min_amount" id="min_amount" class="form-control"
+                                        placeholder="Min Amount">
+                                    <p></p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="status">Status</label>
+                                    <select name="status" id="status" class="form-control">
+                                        <option value="1">Active</option>
+                                        <option value="0">Block</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="starts_at">Starts At</label>
+                                    <input type="text" name="starts_at" id="starts_at" class="form-control"
+                                        placeholder="Starts At">
+                                    <p></p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="expires_at">Expires At</label>
+                                    <input type="text" name="expires_at" id="expires_at" class="form-control"
+                                        placeholder="Expires At">
+                                    <p></p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="description">Description</label>
+                                    <textarea class="form-control" name="description" id="description" cols="30" rows="5"></textarea>
+                                    <p></p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="pb-5 pt-3">
+                    <button type="submit" class="btn btn-primary">Create</button>
+                    <a href="{{ route('categories.index') }}" class="btn btn-outline-dark ml-3">Cancel</a>
+                </div>
+
+            </form>
+
+
+        </div>
+        <!-- /.card -->
+    </section>
+    <!-- /.content -->
+@endsection
+
+@section('customJs')
+    <script>
+        $(document).ready(function() {
+            $('#starts_at').datetimepicker({
+                // options here
+                format: 'Y-m-d H:i:s',
+            });
+        });
+
+        $(document).ready(function() {
+            $('#expires_at').datetimepicker({
+                // options here
+                format: 'Y-m-d H:i:s',
+            });
+        });
+
+        // Web Validation
+        $('#categoryForm').submit(function(event) {
+            event.preventDefault();
+            var element = $(this);
+            $("button[type=submit]").prop('disabled', true);
+
+            $.ajax({
+                url: '{{ route('categories.store') }}',
+                type: 'post',
+                data: element.serializeArray(),
+                dataType: 'json',
+                success: function(response) {
+                    $("button[type=submit]").prop('disabled', false);
+
+                    if (response['status'] == true) {
+
+                        window.location.href = "{{ route('categories.index') }}"
+
+                        $('#name').addClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback').html('');
+
+                        $('#slug').addClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback').html('');
+
+                    } else {
+                        var errors = response['errors'];
+                        if (errors['name']) {
+                            $('#name').addClass('is-invalid')
+                                .siblings('p')
+                                .addClass('invalid-feedback').html(errors['name']);
+                        } else {
+                            $('#name').addClass('is-invalid')
+                                .siblings('p')
+                                .removeClass('invalid-feedback').html('');
+                        }
+
+                        if (errors['slug']) {
+                            $('#slug').addClass('is-invalid')
+                                .siblings('p')
+                                .addClass('invalid-feedback').html(errors['slug']);
+                        } else {
+                            $('#slug').addClass('is-invalid')
+                                .siblings('p')
+                                .removeClass('invalid-feedback').html('');
+                        }
+                    }
+
+
+
+                },
+                error: function(jqXHR, exception) {
+                    console.log('Something went worng');
+                }
+            })
+        });
+
+
+        // Slug name creater
+        $('#name').change(function() {
+            element = $(this);
+            $("button[type=submit]").prop('disabled', true);
+
+            $.ajax({
+                url: '{{ route('getSlug') }}',
+                type: 'get',
+                data: {
+                    title: element.val()
+                },
+                dataType: 'json',
+                success: function(response) {
+                    $("button[type=submit]").prop('disabled', false);
+
+                    if (response['status'] == true) {
+                        $('#slug').val(response['slug']);
+                    }
+                }
+            });
+        });
+
+
+        // IMAGE UPLOAD FUNCTION
+
+        Dropzone.autoDiscover = false;
+        const dropzone = $("#image").dropzone({
+            init: function() {
+                this.on('addedfile', function(file) {
+                    if (this.files.length > 1) {
+                        this.removeFile(this.files[0]);
+                    }
+                });
+            },
+            url: "{{ route('temp-images.create') }}",
+            maxFiles: 1,
+            paramName: 'image',
+            addRemoveLinks: true,
+            acceptedFiles: "image/jpeg,image/png,image/gif",
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success: function(file, response) {
+                $("#image_id").val(response.image_id);
+                //console.log(response)
+            }
+        });
+    </script>
+@endsection
