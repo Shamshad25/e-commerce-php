9bbee8b1d10fbefef14ebbdc035c95aae279d863
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-17 23:22:48 +0530

    Edit route added

--- resources/views/admin/coupon/list.blade.php
+++ resources/views/admin/coupon/list.blade.php
@@ -96,7 +96,7 @@
 
                                         </td>
                                         <td>
-                                            <a href="{{ route('categories.edit', $discountCoupon->id) }}">
+                                            <a href="{{ route('coupons.edit', $discountCoupon->id) }}">
                                                 <svg class="filament-link-icon w-4 h-4 mr-1"
                                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                                     fill="currentColor" aria-hidden="true">
