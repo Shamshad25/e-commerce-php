437d35fb6c95498a169ba80dd4f9b2d9914691a1
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-17 23:41:10 +0530

    Update method created

--- app/Http/Controllers/admin/DiscountCodeController.php
+++ app/Http/Controllers/admin/DiscountCodeController.php
@@ -100,10 +100,6 @@ class DiscountCodeController extends Controller
         }
     }
 
-    public function update(){
-
-    }
-
     public function edit(Request $request, $id){
 
         $coupon = DiscountCoupon::find($id);
@@ -118,6 +114,79 @@ class DiscountCodeController extends Controller
         $data['coupon'] = $coupon;
 
         return view('admin.coupon.edit', $data);
+    }
+
+    public function update(Request $request, $id){
+
+        $discountCode = DiscountCoupon::find($id);
+
+        $validator = Validator::make($request->all(),[
+            'code' => 'required',
+            'type' => 'required',
+            'discount_amount' => 'required|numeric',
+            'status' => 'required',
+            'starts_at'         => 'required|date_format:Y-m-d H:i:s|after:today',
+            'expires_at'        => 'required|date_format:Y-m-d H:i:s|after:starts_at'
+        ]);
+
+        if($validator->passes()){
+
+            // Starting date must be greated than current date
+            // if(!empty($request->starts_at)){
+            //     $now = Carbon::now();
+            //     $startAt = Carbon::createFromFormat('Y-m-d H:i:s',$request->starts_at);
+
+            //     // let = less than equal to
+            //     if($startAt->lte($now) == true){
+            //         return response()->json([
+            //             'status' => false,
+            //             'errors' => ['starts_at' => 'Start date cannot be less than current time.'],
+            //         ]);
+            //     }
+            // }
+
+
+            // // Expiry date must be greated than start date
+            // if(!empty($request->starts_at) && !empty($request->expires_at)){
+            //     $expiresAt = Carbon::createFromFormat('Y-m-d H:i:s',$request->expires_at);
+            //     $startAt = Carbon::createFromFormat('Y-m-d H:i:s',$request->starts_at);
+
+            //     // let = less than equal to
+            //     if($expiresAt->gt($startAt) == false){
+            //         return response()->json([
+            //             'status' => false,
+            //             'errors' => ['expires_at' => 'Expiry date must be greater than start date.'],
+            //         ]);
+            //     }
+            // }
+
+            $discountCode->code = $request->code;
+            $discountCode->name = $request->name;
+            $discountCode->description = $request->description;
+            $discountCode->max_uses = $request->max_uses;
+            $discountCode->max_uses_user = $request->max_uses_user;
+            $discountCode->type = $request->type;
+            $discountCode->discount_amount = $request->discount_amount;
+            $discountCode->min_amount = $request->min_amount;
+            $discountCode->status = $request->status;
+            $discountCode->starts_at = $request->starts_at;
+            $discountCode->expires_at = $request->expires_at;
+            $discountCode->save();
+
+            session()->flash('success','Discount coupon added successfully.');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Discount coupon added successfully.',
+            ]);
+
+        }else{
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+
     }
 
     public function destroy(){
