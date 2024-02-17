8fa028fad16599c09c2197050811c245d43100d1
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-17 20:36:29 +0530

    Date time picker script added

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -19,6 +19,8 @@
 
     <link rel="stylesheet" href="{{ asset('admin-assets/plugins/select2/css/select2.min.css') }}">
 
+    <link rel="stylesheet" href="{{ asset('admin-assets/css/datetimepicker.css') }}">
+
     <link rel="stylesheet" href="{{ asset('admin-assets/css/custom.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
@@ -100,6 +102,8 @@
     <script src="{{ asset('admin-assets/plugins/summernote/summernote.min.js') }}"></script>
 
     <script src="{{ asset('admin-assets/plugins/select2/js/select2.min.js') }}"></script>
+
+    <script src="{{ asset('admin-assets/js/datetimepicker.js') }}"></script>
 
     <!-- AdminLTE for demo purposes -->
     <script src="{{ asset('admin-assets/js/demo.js') }}"></script>
