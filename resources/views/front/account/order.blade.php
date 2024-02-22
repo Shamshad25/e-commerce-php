571f605bbd2844664a88ffc470d9b2ab92741227
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-22 22:53:00 +0530

    My profile link added to order page

--- resources/views/front/account/order.blade.php
+++ resources/views/front/account/order.blade.php
@@ -5,8 +5,8 @@
         <div class="container">
             <div class="light-font">
                 <ol class="breadcrumb primary-color mb-0">
-                    <li class="breadcrumb-item"><a class="white-text" href="#">My Account</a></li>
-                    <li class="breadcrumb-item">Settings</li>
+                    <li class="breadcrumb-item"><a class="white-text" href="{{ route('account.profile') }}">My Account</a></li>
+                    <li class="breadcrumb-item">My Orders</li>
                 </ol>
             </div>
         </div>
