ba5bd1c3412959fb2e40175ca81c4a844b109f41
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-22 22:51:42 +0530

    Order tempelate created

--- resources/views/front/account/order.blade.php
+++ resources/views/front/account/order.blade.php
@@ -22,10 +22,67 @@
                 <div class="col-md-9">
                     <div class="card">
                         <div class="card-header">
-                            <h2 class="h5 mb-0 pt-2 pb-2">Personal Information</h2>
+                            <h2 class="h5 mb-0 pt-2 pb-2">My Orders</h2>
                         </div>
                         <div class="card-body p-4">
-                            
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Orders #</th>
+                                            <th>Date Purchased</th>
+                                            <th>Status</th>
+                                            <th>Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <a href="order-detail.php">OR756374</a>
+                                            </td>
+                                            <td>11 Nav, 2022</td>
+                                            <td>
+                                                <span class="badge bg-success">Delivered</span>
+
+                                            </td>
+                                            <td>$400</td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a href="order-detail.php">OR756374</a>
+                                            </td>
+                                            <td>10 Oct, 2022</td>
+                                            <td>
+                                                <span class="badge bg-success">Delivered</span>
+
+                                            </td>
+                                            <td>$400</td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a href="order-detail.php">OR756374</a>
+                                            </td>
+                                            <td>02 Sep, 2022</td>
+                                            <td>
+                                                <span class="badge bg-success">Delivered</span>
+
+                                            </td>
+                                            <td>$400</td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a href="order-detail.php">OR756374</a>
+                                            </td>
+                                            <td>01 Dec, 2022</td>
+                                            <td>
+                                                <span class="badge bg-success">Delivered</span>
+
+                                            </td>
+                                            <td>$400</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
