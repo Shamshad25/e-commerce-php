9b0ffb97dfde98b642b2ad665edce60b83f5719d
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-02-29 18:53:03 +0530

    Bug fixed fetching order

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function index(){
-        return view('admin.orders.list');
+    public function index(Request $request){
+
+
+        $orders = Order::latest('orders.created_at')->select('orders.*','users.name', 'users.email');
+
+        $orders = $orders->leftJoin('users', 'users.id', 'orders.user_id');
+
+
+        // FOR SEARCHING
+        if($request->get('keyword') != ""){
+            $orders = $orders->where('users.name', 'like', '%'.$request->keyword.'%');
+            $orders = $orders->orWhere('users.email', 'like', '%'.$request->keyword.'%');
+            $orders = $orders->orWhere('orders.id', 'like', '%'.$request->keyword.'%');
+        }
+
+        $orders = $orders->paginate(10);
+
+        return view('admin.orders.list',[
+            'orders' => $orders
+        ]);
     }
 
     public function derail(){
