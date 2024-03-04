64e3d48d142a05624880f03e076667968ffea479
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-04 22:13:00 +0530

    send email modified

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -347,7 +347,7 @@ class CartController extends Controller
         }
 
         // Send Order Email
-        orderEmail($order->id);
+        orderEmail($order->id, 'customer');
 
         session()->flash('success', 'You have successfully placed your order.');
 
