6fb54e0ee577e78fe4fb90ea425b481d10cf6658
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-05 11:57:54 +0530

    Addto wishlist method added

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Wishlist;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FrontController extends Controller
 {
@@ -17,5 +19,38 @@ class FrontController extends Controller
         $data['latestProducts'] = $latestProducts;
 
         return view('front.home',$data);
+    }
+
+    public function addToWishlist(Request $request){
+        if(Auth::check() == false){
+
+            session(['url.intended' => url()->previous()]);
+
+            return response()->json([
+                'status' => false,
+
+            ]);
+        }
+
+        $product = Product::where('id', $request->id)->first();
+
+        if($product == null){
+            return response()->json([
+                'status' => true,
+                'message' => '<div class="alert alert-danger">Product not found.</div>'
+            ]);
+        }
+
+        $wishlist = new Wishlist();
+        $wishlist->user_id = Auth::user()->id;
+        $wishlist->product_id = $request->id;
+        $wishlist->save();
+
+
+        return response()->json([
+            'status' => true,
+            'message' => '<div class="alert alert-success"><string>"'.$product->title.'"</string> added to your wishlist.</div>'
+        ]);
+
     }
 }
