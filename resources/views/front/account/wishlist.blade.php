297880565f8a917242b57b5fa2c28399a03dbc6a
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-06 21:38:51 +0530

    wishlist page created

---
+++ resources/views/front/account/wishlist.blade.php
@@ -0,0 +1,35 @@
+@extends('front.layouts.app')
+
+@section('content')
+    <section class="section-5 pt-3 pb-3 mb-3 bg-white">
+        <div class="container">
+            <div class="light-font">
+                <ol class="breadcrumb primary-color mb-0">
+                    <li class="breadcrumb-item"><a class="white-text" href="#">My Account</a></li>
+                    <li class="breadcrumb-item">My Wishlist</li>
+                </ol>
+            </div>
+        </div>
+    </section>
+
+    <section class=" section-11 ">
+        <div class="container  mt-5">
+            <div class="row">
+                <div class="col-md-3">
+                    @include('front.account.common.sidebar')
+
+                </div>
+                <div class="col-md-9">
+                    <div class="card">
+                        <div class="card-header">
+                            <h2 class="h5 mb-0 pt-2 pb-2">My Wishlist</h2>
+                        </div>
+                        <div class="card-body p-4">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
