18cd1b0671a731cbe125cca33e2b30589314295e
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-06 22:44:44 +0530

    Status updated

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -90,7 +90,7 @@ class ShopController extends Controller
         if($product->related_products != ''){
             $productArray = explode(',',$product->related_products);
 
-            $relatedProducts = Product::whereIn('id',$productArray)->get();
+            $relatedProducts = Product::whereIn('id',$productArray)->where('status', 1)->get();
         }
 
         $data['product'] = $product;
