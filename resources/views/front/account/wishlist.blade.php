cb6ab0fa2ccfb86dfb973eccd04b9bdd70b09fff
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-06 21:58:24 +0530

    wishlist data made dynamic

--- resources/views/front/account/wishlist.blade.php
+++ resources/views/front/account/wishlist.blade.php
@@ -25,7 +25,49 @@
                             <h2 class="h5 mb-0 pt-2 pb-2">My Wishlist</h2>
                         </div>
                         <div class="card-body p-4">
+                            @if ($wishlists->isNotEmpty())
+                                @foreach ($wishlists as $wishlist)
+                                    <div class="d-sm-flex justify-content-between mt-lg-4 mb-4 pb-3 pb-sm-2 border-bottom">
+                                        <div class="d-block d-sm-flex align-items-start text-center text-sm-start">
 
+
+                                            @php
+                                                $productImage = getProductImage($wishlist->product_id);
+                                            @endphp
+
+
+                                            <a class="d-block flex-shrink-0 mx-auto me-sm-4"
+                                                href="{{ route('front.product', $wishlist->product->slug) }}"
+                                                style="width: 10rem;">
+                                                @if (!empty($productImage))
+                                                    <img
+                                                        src="{{ asset('uploads/product/small/' . $productImage->image) }}" />
+                                                @else
+                                                    <img src="{{ asset('admin-assets/img/default-150x150.png') }}"
+                                                        alt="" />
+                                                @endif
+                                            </a>
+                                            <div class="pt-2">
+                                                <h3 class="product-title fs-base mb-2"><a
+                                                        href="{{ route('front.product', $wishlist->product->slug) }}">{{ $wishlist->product->title }}</a>
+                                                </h3>
+                                                <div class="fs-lg text-accent pt-2">
+                                                    <span
+                                                        class="h5"><strong>${{ $wishlist->product->price }}</strong></span>
+                                                    @if ($wishlist->product->compare_price > 0)
+                                                        <span
+                                                            class="h6 text-underline"><del>${{ $wishlist->product->compare_price }}</del></span>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="pt-2 ps-sm-3 mx-auto mx-sm-0 text-center">
+                                            <button class="btn btn-outline-danger btn-sm" type="button"><i
+                                                    class="fas fa-trash-alt me-2"></i>Remove</button>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            @endif
                         </div>
                     </div>
                 </div>
