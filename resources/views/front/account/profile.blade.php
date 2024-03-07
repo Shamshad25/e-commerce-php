9a5b5334b4a9da84f66dd1067b698947fd1f6d1e
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-07 14:39:49 +0530

    Update profile form complete

--- resources/views/front/account/profile.blade.php
+++ resources/views/front/account/profile.blade.php
@@ -15,6 +15,9 @@
     <section class=" section-11 ">
         <div class="container  mt-5">
             <div class="row">
+                <div class="col-md-12">
+                    @include('front.account.common.message')
+                </div>
                 <div class="col-md-3">
                     @include('front.account.common.sidebar')
 
@@ -24,38 +27,102 @@
                         <div class="card-header">
                             <h2 class="h5 mb-0 pt-2 pb-2">Personal Information</h2>
                         </div>
-                        <div class="card-body p-4">
-                            <div class="row">
-                                <div class="mb-3">
-                                    <label for="name">Name</label>
-                                    <input type="text" name="name" id="name" placeholder="Enter Your Name"
-                                        class="form-control">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="email">Email</label>
-                                    <input type="text" name="email" id="email" placeholder="Enter Your Email"
-                                        class="form-control">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="phone">Phone</label>
-                                    <input type="text" name="phone" id="phone" placeholder="Enter Your Phone"
-                                        class="form-control">
-                                </div>
+                        <form action="" name="profileForm" id="profileForm">
+                            <div class="card-body p-4">
+                                <div class="row">
+                                    <div class="mb-3">
+                                        <label for="name">Name</label>
+                                        <input value="{{ $user->name }}" type="text" name="name" id="name"
+                                            placeholder="Enter Your Name" class="form-control">
+                                        <p></p>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="email">Email</label>
+                                        <input value="{{ $user->email }}" type="text" name="email" id="email"
+                                            placeholder="Enter Your Email" class="form-control">
+                                        <p></p>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="phone">Phone</label>
+                                        <input value="{{ $user->phone }}" type="text" name="phone" id="phone"
+                                            placeholder="Enter Your Phone" class="form-control">
+                                        <p></p>
+                                    </div>
 
-                                <div class="mb-3">
+                                    {{-- <div class="mb-3">
                                     <label for="phone">Address</label>
                                     <textarea name="address" id="address" class="form-control" cols="30" rows="5"
                                         placeholder="Enter Your Address"></textarea>
-                                </div>
+                                </div> --}}
 
-                                <div class="d-flex">
-                                    <button class="btn btn-dark">Update</button>
+                                    <div class="d-flex">
+                                        <button class="btn btn-dark">Update</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+@endsection
+@section('customJs')
+    <script>
+        $('#profileForm').submit(function(event) {
+            event.preventDefault();
+
+            $.ajax({
+                url: "{{ route('account.updateProfile') }}",
+                type: 'post',
+                data: $(this).serializeArray(),
+                dataType: 'json',
+                success: function(response) {
+                    if (response.status == true) {
+
+                        $('#name').removeClass('is-invalid').siblings('p').html('')
+                            .removeClass('invalid-feedback');
+
+                        $('#email').removeClass('is-invalid').siblings('p').html('')
+                            .removeClass('invalid-feedback');
+
+                        $('#phone').removeClass('is-invalid').siblings('p').html('')
+                            .removeClass('invalid-feedback');
+
+                        window.location.href = "{{ route('account.profile') }}"
+
+
+                    } else {
+                        var errors = response.errors;
+
+
+                        if (errors.name) {
+                            $('#name').addClass('is-invalid').siblings('p').html(errors.name)
+                                .addClass('invalid-feedback');
+                        } else {
+                            $('#name').removeClass('is-invalid').siblings('p').html('')
+                                .removeClass('invalid-feedback');
+                        }
+
+                        if (errors.email) {
+                            $('#email').addClass('is-invalid').siblings('p').html(errors.email)
+                                .addClass('invalid-feedback');
+                        } else {
+                            $('#email').removeClass('is-invalid').siblings('p').html('')
+                                .removeClass('invalid-feedback');
+                        }
+
+                        if (errors.phone) {
+                            $('#phone').addClass('is-invalid').siblings('p').html(errors.phone)
+                                .addClass('invalid-feedback');
+                        } else {
+                            $('#phone').removeClass('is-invalid').siblings('p').html('')
+                                .removeClass('invalid-feedback');
+                        }
+
+                    }
+                }
+            });
+        });
+    </script>
 @endsection
