6cfcc9d35dff6dac0060af31afb9288f59fd9ec5
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-08 13:29:13 +0530

    store method complete

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -32,9 +33,33 @@ class UserController extends Controller
     public function store(Request $request){
         $validator = Validator::make($request->all(),[
             'name' => 'required',
+            'password' => 'required|min:5',
             'email' => 'required|email|unique:users',
             'phone' => 'required'
         ]);
+
+        if($validator->passes()){
+
+            $user = new User;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->phone = $request->phone;
+            $user->save();
+
+            session()->flash('success', 'User created successfully.');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User created successfully.',
+            ]);
+
+        }else{
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
     }
 
 }
