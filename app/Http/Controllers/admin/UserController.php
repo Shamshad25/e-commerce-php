e7d13383528422fd9b4a693bcd0cc320928bac1a
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-08 13:04:15 +0530

    create method added

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
 
         if(!empty($request->get('keyword'))){
             $users = $users->where('name', 'like', '%'.$request->get('keyword').'%');
-            $users = $users->where('email', 'like', '%'.$request->get('keyword').'%');
+            $users = $users->orWhere('email', 'like', '%'.$request->get('keyword').'%');
         }
 
         $users = $users->paginate(10);
@@ -23,4 +23,9 @@ class UserController extends Controller
             'users' => $users
         ]);
     }
+
+    public function create(Request $request){
+        return view('admin.users.create');
+    }
+
 }
