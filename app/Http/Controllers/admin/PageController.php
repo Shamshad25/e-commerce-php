14c013cdecbedaf259a4356d811e36c4a6d16d08
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-09 23:02:45 +0530

    store method complete

--- app/Http/Controllers/admin/PageController.php
+++ app/Http/Controllers/admin/PageController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Page;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PageController extends Controller
 {
@@ -15,7 +17,33 @@ class PageController extends Controller
         return view('admin.pages.create');
     }
 
-    public function store(){
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'slug' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $page = new Page;
+        $page->name = $request->name;
+        $page->slug = $request->slug;
+        $page->content = $request->content;
+        $page->save();
+
+        $message  = 'Page created successfully.';
+
+        session()->flash('success', $message);
+
+        return response()->json([
+            'status' => true,
+            'message' => $message
+        ]);
 
     }
 
