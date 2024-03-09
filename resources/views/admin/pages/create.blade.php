d3d4f8e712a9c828ef350d143e167c16d8d56bd6
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-09 23:19:56 +0530

    Create page complete

--- resources/views/admin/pages/create.blade.php
+++ resources/views/admin/pages/create.blade.php
@@ -9,7 +9,7 @@
                     <h1>Create Page</h1>
                 </div>
                 <div class="col-sm-6 text-right">
-                    <a href="{{ route('categories.index') }}" class="btn btn-primary">Back</a>
+                    <a href="{{ route('pages.index') }}" class="btn btn-primary">Back</a>
                 </div>
             </div>
         </div>
@@ -55,7 +55,7 @@
 
                 <div class="pb-5 pt-3">
                     <button type="submit" class="btn btn-primary">Create</button>
-                    <a href="{{ route('categories.index') }}" class="btn btn-outline-dark ml-3">Cancel</a>
+                    <a href="{{ route('pages.index') }}" class="btn btn-outline-dark ml-3">Cancel</a>
                 </div>
 
             </form>
@@ -85,8 +85,6 @@
 
                     if (response['status'] == true) {
 
-                        window.location.href = "{{ route('pages.index') }}"
-
                         $('#name').removeClass('is-invalid')
                             .siblings('p')
                             .removeClass('invalid-feedback').html('');
@@ -94,6 +92,9 @@
                         $('#slug').removeClass('is-invalid')
                             .siblings('p')
                             .removeClass('invalid-feedback').html('');
+
+                        window.location.href = "{{ route('pages.index') }}"
+
 
                     } else {
                         var errors = response['errors'];
