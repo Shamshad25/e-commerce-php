b9ffbee53aa9c040d50ae690e434c3aea6e60aa6
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-10 00:13:00 +0530

    Delete page route created

--- routes/web.php
+++ routes/web.php
@@ -187,7 +187,7 @@ Route::group(['prefix' => 'admin'], function(){
         Route::post('/pages',[PageController::class,'store'])->name('pages.store');
         Route::get('/pages/{page}/edit',[PageController::class,'edit'])->name('pages.edit');
         Route::put('/pages/{page}',[PageController::class,'update'])->name('pages.update');
-        // Route::delete('/users/{user}',[PageController::class,'destroy'])->name('users.delete');
+        Route::delete('/pages/{page}',[PageController::class,'destroy'])->name('pages.delete');
 
 
 
