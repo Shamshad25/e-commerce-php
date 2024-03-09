a4420fe5003bc7d77359108bfbb6d77540bb248d
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-09 20:38:43 +0530

    create method added to page controller

--- app/Http/Controllers/admin/PageController.php
+++ app/Http/Controllers/admin/PageController.php
@@ -7,5 +7,28 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        return view('admin.pages.index');
+    }
+
+    public function create(){
+        return view('admin.pages.create');
+    }
+
+    public function store(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function destroy(){
+
+    }
+
 }
