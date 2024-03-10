e79627f5699e4d04923a1a1faef71d2a677d46f6
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-10 20:31:48 +0530

    change password route updated

--- resources/views/front/account/common/sidebar.blade.php
+++ resources/views/front/account/common/sidebar.blade.php
@@ -12,8 +12,8 @@
             aria-controls="tab-register" aria-expanded="false"><i class="fas fa-heart"></i> Wishlist</a>
     </li>
     <li class="nav-item">
-        <a href="change-password.php" class="nav-link font-weight-bold" role="tab" aria-controls="tab-register"
-            aria-expanded="false"><i class="fas fa-lock"></i> Change Password</a>
+        <a href="{{ route('account.changePassword') }}" class="nav-link font-weight-bold" role="tab"
+            aria-controls="tab-register" aria-expanded="false"><i class="fas fa-lock"></i> Change Password</a>
     </li>
     <li class="nav-item">
         <a href="{{ route('account.logout') }}" class="nav-link font-weight-bold" role="tab"
