0074e07c9b448b86f3353ad84871f195d97f334f
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-11 18:41:40 +0530

    forgotPassword method added to controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ResetPasswordEmail;
 use App\Models\Country;
 use App\Models\CustomerAddress;
 use App\Models\Order;
@@ -10,8 +11,11 @@ use App\Models\User;
 use App\Models\Wishlist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -292,6 +296,42 @@ class AuthController extends Controller
                 'errors' => $validator->errors()
             ]);
         }
+    }
+
+    public function forgotPassword(){
+        return view('front.account.forgot-password');
+    }
+
+    public function processForgotPassword(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email|exists:users,email',
+        ]);
+
+        if($validator->fails()){
+            // dd('yo');
+            return redirect()->route('front.forgotPassword')->withInput()->withErrors($validator);
+        }
+
+        $token = Str::random(60);
+
+        \DB::table('password_reset_tokens')->insert([
+            'email' => $request->email,
+            'token' => $token,
+            'created_at' => now(),
+        ]);
+
+        // Send email here
+
+        $user = User::where('email', $request->email)->first();
+
+        $formData = [
+            'token' => $token,
+            'user' => $user,
+            'mail_subject' => 'You have requested to reset your password'
+        ];
+
+        Mail::to($request->email)->send(new ResetPasswordEmail($formData));
+
     }
 
 }
