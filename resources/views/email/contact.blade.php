27c5a59d8d2145082991b6389c8c7c1fd0e90b36
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-03-11 13:33:32 +0530

    contact mail page complete

--- resources/views/email/contact.blade.php
+++ resources/views/email/contact.blade.php
@@ -5,10 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Order Email</title>
+    <title>Contact Email</title>
 </head>
 
 <body style="font-family: Arial, Helvetica, sans-serif; font-size: 16px">
+    <h1>You have recieved a contact email.</h1>
+
+    <p>Name: {{ $mailData['name'] }}</p>
+    <p>Email: {{ $mailData['email'] }}</p>
+    <p>Subject: {{ $mailData['subject'] }}</p>
+
+    <p>Message:</p>
+    <p>{{ $mailData['message'] }}</p>
 </body>
 
 </html>
